4cf2c863d959766b3b432dfb7a1a9fde928aa15f
maheshnihar <maheshnihar@gmail.com>
2020-02-27 22:04:40 -0800

    Updates

---
+++ precache-manifest.73eb924b2bd1ef9a379e3e06d6de5b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35df625e0c2b6bd0b98657131fcb958e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9a267144675935d684b7",
+    "url": "/portfolio/static/css/main.981d7053.chunk.css"
+  },
+  {
+    "revision": "f8767f33ff36f430407d",
+    "url": "/portfolio/static/js/2.207adced.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.207adced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a267144675935d684b7",
+    "url": "/portfolio/static/js/main.71160b50.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "3fb37dca60b4846992e6cdd92cde818f",
+    "url": "/portfolio/static/media/thor.3fb37dca.png"
+  }
+]);
