9df8cf0e5965622955dd77668c3a20bd56ae88e5
maheshnihar <maheshnihar@gmail.com>
2020-02-28 00:27:40 -0800

    added css transistion to mobile drawer

--- portfolio/src/Components/MobileMenuDrawer.tsx
+++ portfolio/src/Components/MobileMenuDrawer.tsx
@@ -12,24 +12,22 @@ const MobileMenuDrawer: React.FunctionComponent<MobileMenuDrawerProps> = (props)
     const [visible, SetDrawer] = useState(props.open);
 
     return (
-        <div>
-            <CSSTransition
-                in={visible}
-                timeout={350}
-                classNames="list-transition"
-                unmountOnExit
-                appear
-            >
-                <div id="mySidenav" className="sidenav">
-                    <a href="javascript:void(0)" className="closebtn" onClick={props.closeDrawer} >&times;</a>
-                    <a href='#'>Summary</a>
-                    <a href='#'>Experience</a>
-                    <a href='#'>Projects</a>
-                    <a href='#'>Skills</a>
-                    <a href='#'>Contact</a>
-                </div>
-            </CSSTransition>
-        </div>
+        <CSSTransition
+            in={true}
+            timeout={350}
+            classNames="display"
+            unmountOnExit
+            appear
+        >
+            <div id="mySidenav" className="sidenav">
+                <a href="javascript:void(0)" className="closebtn" onClick={props.closeDrawer} >&times;</a>
+                <a href='#'>Summary</a>
+                <a href='#'>Experience</a>
+                <a href='#'>Projects</a>
+                <a href='#'>Skills</a>
+                <a href='#'>Contact</a>
+            </div>
+        </CSSTransition>
     )
 }
 
